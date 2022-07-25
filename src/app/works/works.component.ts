c7d290630869c7e1afe2dd6146770b0c8a361d0d
PedroSalvino <pedro_luiz7199@outlook.com>
2022-07-25 11:20:05 -0300

    6 files modified, 1 added

--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-works',
   templateUrl: './works.component.html',
-  styleUrls: ['./works.component.css']
+  styleUrls: ['./works.component.css'],
 })
 export class WorksComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
